de6c27fef3b35b57c72a240156e36d91afad77bd
josepmartorell <martorelljosep>
2022-08-24 23:28:41 +0200

    start project

---
+++ src/main/java/com/crud/sql/RestApiResearchGroupJwtApplication.java
@@ -0,0 +1,13 @@
+package com.crud.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiResearchGroupJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiResearchGroupJwtApplication.class, args);
+	}
+
+}
